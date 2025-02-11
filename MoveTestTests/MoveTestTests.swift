88f96a9a8cda6dabaf604b2a361c82e26f053499
JeanZ <ZJean9@outlook.com>
2025-02-10 22:03:47 -0500

    Initial Commit

---
+++ MoveTestTests/MoveTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoveTestTests.swift
+//  MoveTestTests
+//
+//  Created by Jeanzhao on 2/10/25.
+//
+
+import Testing
+@testable import MoveTest
+
+struct MoveTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
