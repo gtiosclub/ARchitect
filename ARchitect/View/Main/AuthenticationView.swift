5c4b42bd48c9d4c3aa036aaff59cee2aa6b267c7
p-joshi7 <preyasjoshi2005@gmail.com>
2025-02-20 19:12:05 -0500

    Login UI updated and sign up view created

--- ARchitect/View/Main/AuthenticationView.swift
+++ ARchitect/View/Main/AuthenticationView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct AuthenticationView: View {
-    @Binding var isAuthenticated: Bool
+    @Binding var isAuthenticated: Bool;
+    @State private var name: String = "";
+    @State private var password: String = "";
+    
     var body: some View {
         VStack {
-            Text("AuthenticationView Placeholder")
-            Button("Press to authenticate") {
+            Text("AR").font(.largeTitle).fontWeight(.bold)
+            + Text("chitect").font(.largeTitle).fontWeight(.thin)
+            
+            TextField("Username/Email", text: $name)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+            
+            SecureField("Password", text: $password)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+            
+            Button(action: {
                 isAuthenticated = true
+            }) {
+                Text("Login")
+                    .font(.headline)
+                    .foregroundColor(.black)
+                    .padding()
+                    .frame(width: 200, height: 50) // Adjust size to create an oval shape
+                    .background(Color.white) // Button background color
+                    .clipShape(Capsule()) // Makes it an oval
+                    .overlay(
+                        Capsule().stroke(Color.black, lineWidth: 2) // Black outline
+                    )
             }
         }
         
