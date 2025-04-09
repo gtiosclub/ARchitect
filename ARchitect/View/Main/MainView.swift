7befeeab709aedf070d1c0d3d7f77559d059f711
ronitjain04 <ronit.jain@gmail.com>
2025-04-09 05:06:13 -0400

    fix

--- ARchitect/View/Main/MainView.swift
+++ ARchitect/View/Main/MainView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct MainView: View {
-    @State private var isAuthenticated: Bool = true
+    @State private var isAuthenticated: Bool = false
     @State private var selectedTab = 0
     
     var body: some View {
